3347ec6e700a5d9dae5147b8aebb2adc52e273f7
marko154 <marko.gartnar777@gmail.com>
2024-12-20 12:18:25 +0100

    implement buffered writing of T records in RecordWriter

--- internal/records.go
+++ internal/records.go
@@ -7,13 +7,24 @@ import (
 	"io"
 )
 
+const (
+	T_RECORD_MAX_SIZE = (1 << 16) - 1
+)
+
+type TRecord struct {
+	address int
+	text    []byte
+}
+
 type RecordWriter struct {
-	writer *bufio.Writer
+	writer      *bufio.Writer
+	currTRecord *TRecord
 }
 
 func NewRecordWriter(writer io.Writer) *RecordWriter {
 	return &RecordWriter{
-		writer: bufio.NewWriter(writer),
+		writer:      bufio.NewWriter(writer),
+		currTRecord: &TRecord{0, []byte{}},
 	}
 }
 
@@ -47,10 +58,34 @@ func (w *RecordWriter) WriteMRecord(address, nibbles int) {
 }
 
 func (w *RecordWriter) WriteCode(address int, bytes []byte) {
-	// TODO: implement
+	// flush current T record if there is a gap
+	lastAddress := w.currTRecord.address + len(w.currTRecord.text)
+	if address > lastAddress {
+		w.flushTRecord()
+		w.currTRecord = &TRecord{address, []byte{}}
+	}
+
+	// append to last record, flush when full
+	remainingBytes := len(bytes)
+	for remainingBytes > 0 {
+		remainingSpace := T_RECORD_MAX_SIZE - len(w.currTRecord.text)
+		bytesToWrite := min(remainingSpace, remainingBytes)
+		remainingBytes -= bytesToWrite
+
+		if len(w.currTRecord.text) == T_RECORD_MAX_SIZE {
+			w.flushTRecord()
+			address += T_RECORD_MAX_SIZE
+			w.currTRecord = &TRecord{address, []byte{}}
+		}
+	}
 }
 
-func (w *RecordWriter) WriteTRecord(address int, bytes []byte) {
+func (w *RecordWriter) flushTRecord() {
+	address := w.currTRecord.address
+	bytes := w.currTRecord.text
+	if len(bytes) == 0 {
+		return
+	}
 	w.writeLine(
 		fmt.Sprintf("T%06X%02X%s", address, len(bytes), hex.EncodeToString(bytes)),
 	)
